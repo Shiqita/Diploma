5b43897d091258c59696a24188952775b600113e
Denis <mrdenin2000@mail.com>
2022-06-05 00:28:23 +1000

    it's working, but too slow.

--- mainfun.m
+++ mainfun.m
@@ -1,7 +1,7 @@
 %function mainfun()
 %initial vars
 
-xi=(2:5:100);
+xi=(0:0.5:10);
 
 %sobstvenniey zna4eniya
 h=5; 
@@ -15,43 +15,47 @@ n=length(xi);
 %eps=;
 
 %% setup 
-%r=zeros(n,1);
+temp_R=set_R(w,ro,l,h); %Set R1 R2 ^R2
+arg=[0 ro w l h temp_R];
+P1=temp_R(1);
+P2=temp_R(2);
+P3=temp_R(3);
+%-------------
 teta=zeros(n,1);
 omega=zeros(n,1);
 omega(1)=1;
-%L=zeros(n,1);
-%dL=zeros(n,1);
-%-------------
-arg=[0 ro w l h];
 teta(1)=pi/2-v2(arg);                                          
 %-------------
-
-%r_=zeros(n,1);
 teta_=zeros(n,1);
 omega_=zeros(n,1);
 omega_(n)=1;
-%L_=zeros(n,1);
-%dL_=zeros(n,1);
-%-------------
 teta_(n)=-pi/2;
 %-------------
 
 X=set_x(xi,ro); % poly4aem iz XI
 
-%NormControl 'on' 'off' - mnogocomponentnoe vi4islene oshibki|refine n? 'RelTol',eps,
-options=odeset('Refine',4,'Stats','on');
+%NormControl 'on' 'off' - mnogocomponentnoe vi4islene oshibki|refine n?
+%'RelTol',eps, 'Refine',4,
+options=odeset('Stats','on');
 
 %%
 %TETA_OMEGA ->
 for i=1:n-1
     tic
     disp(i)
-    sol=ode45(@(x,y) set_teta_omega(x,y,ro,w,h,l),(X(i):X(i+1)),[teta(i) 1],options);
+    y0=[teta(i) 1];
+    tspan=[X(i) X(i+1)];
+    disp(tspan);
+    
+    sol=ode45(@(x,y) set_teta_omega(x,y,ro,w,l,h,P1,P2,P3),tspan,y0,options);
     temp=deval(sol,sol.x);
     omega(i+1)=temp(2,end);
     teta(i+1)=temp(1,end);
-    disp(omega);
-    disp(teta);
+    %[x,y]=ode45(@(x,y) set_teta_omega(x,y,ro,w,l,h,P1,P2,P3),tspan,y0,options);
+    %omega(i+1)=y(2,end);
+    %teta(i+1)=y(1,end);
+    disp(omega(i+1));
+    disp(teta(i+1));
     toc
 end
 
@@ -71,7 +75,12 @@ toc
 tic
 for i=n-1:1
     tic
-    sol_=ode45(@(x,y) set_teta_omega(x,y,ro,w,h,l),(X(i+1):X(i)),[teta_(i+1) 1],options);
+    disp(i)
+    y0=[teta(i+1) 1];
+    tspan=[X(i+1) X(i)];
+    disp(tspan);
+    
+    sol_=ode45(@(x,y) set_teta_omega(x,y,ro,w,l,h,P1,P2,P3),tspan,y0,options);
     temp=deval(sol_,sol_.x);
     omega_(i)=temp(2,end);
     teta_(i)=temp(1,end);
@@ -120,24 +129,22 @@ function res=df2(xx,roo)
     res=double(subs(fx2,{x,ro},{xx, roo}));
 end
 %% q|U|psi podgotovka k TETA|OMEGA
-function qx=q(x,ro,h,l,w)
-    qx=h*ro^2-l*(ro^2)*(x^2+ro^2)+(w^2)*(x^2+ro^2)^2;
+function res=q(x,ro,h,l,w)
+    res=h*ro^2-l*(ro^2)*(x^2+ro^2)+(w^2)*(x^2+ro^2)^2;
 end
 
-function ux=U(arg)
+function res=U(arg)
     x=arg(1);ro=arg(2);w=arg(3);l=arg(4);h=arg(5);
 
     p1=q(x,h,l,ro,w)/(f(x,ro))^2;
     p2=-(df2(x,ro))/(2*f(x,ro));
     p3=(df1(x,ro)^2)/(4*f(x,ro)^2);
-    ux=p1+p2+p3;
+    res=p1+p2+p3;
 end
 
-%peredaem w-omega o-teta(end)
-function ps=psi(arg,teta)
-    x=arg(1);
-    w=arg(3);
-    ps=w*x+v2(arg)+teta;
+function res=psi(arg,teta)
+    x=arg(1);w=arg(3);
+    res=w*x+v2(arg)+teta;
 end
 %% TETA|OMEGA
 % TETA
@@ -150,31 +157,26 @@ function res=TETA(arg)
     res=p1+p2+p3;
 end
 
-function res=V(arg,tet)
-
+function res=V(arg)
+    tet=arg(end);arg(end)=[];
+    
     p1=(dv1(arg))/(v1(arg))*cos(2*psi(arg,tet));
     p2=((U(arg)/v1(arg)^2)-v1(arg)^2)*sin(2*psi(arg,tet));
     res=(p1+p2)/2;
 end
 
-function res=set_teta_omega(x,y,ro,w,l,h)
-    tic;
-    omega0=y(2);
-    arg=[x ro w l h];
-    %arg=[x args(1) args(4) args(3) args(2)];  
-    %om=args(end);
-    %args(end)=[];
+function res=set_teta_omega(x,y,ro,w,l,h,P1,P2,P3)
+    %tic;
+    omega0=y(2); 
+    arg=[x ro w l h P1 P2 P3];  
     
     teta=TETA([arg y(1)]);
-    omega=-V(arg,teta)*omega0;
-    res=[teta; omega];
-    disp('x(i):');
-    disp(x);
-    disp('teta:');
-    disp(teta);
-    disp('omega:');
-    disp(omega);
-    toc;
+    omega=-V([arg teta])*omega0;
+    res=[teta; omega]; 
+    %show=[x teta omega];
+    %disp('x(i)  teta   omega');
+    %disp(show);
+    %toc;
 end
 %% R|f*Lambda|f'*Lambda' !! start-1 <-  !!!  start+1 -> !!
 function res=R(Omega,start,endl)
@@ -223,45 +225,35 @@ end
 %%                                             end of real functions
 %v1|v2|dv1|dv2
 function res=v1(arg)
-    x=arg(1);ro=arg(2);w=arg(3);l=arg(4);h=arg(5);
-
-    P1=R_1(w,ro,l);
-    P2=R_2_(w,ro,l,h);
-    P=1+P1/(x^2+ro^2)+P2/(x^2+ro^2)^2;
+    x=arg(1);ro=arg(2);w=arg(3);P1=arg(6);P3=arg(8);
+    
+    P=1+P1/(x^2+ro^2)+P3/(x^2+ro^2)^2;
     res=sqrt(w*P);
 end
 
 function res=v2(arg)
-    x=arg(1);ro=arg(2);w=arg(3);l=arg(4);h=arg(5);
+    x=arg(1);ro=arg(2);w=arg(3);P1=arg(6);P3=arg(8);
 
-    P1=R_1(w,ro,l);
-    P2=R_2_(w,ro,l,h);
     p1=(P1/ro)*atan(x/ro);
     tmp=x/(x^2+ro^2)+(1/ro)*atan(x/ro);
-    p2=(P2/(2*ro^2))*tmp;
+    p2=(P3/(2*ro^2))*tmp;
     res=w*(p1+p2);
 end
 
 %dv1=(v1^2)'
 function res=dv1(arg)
-    x=arg(1);ro=arg(2);w=arg(3);l=arg(4);h=arg(5);   
-    
-    P1=R_1(w,ro,l);
-    P2=R_2_(w,ro,l,h);
+    x=arg(1);ro=arg(2);w=arg(3);P1=arg(6);P3=arg(8);   
 
     p1=-(2*P1*x)/(x^2+ro^2)^2;
-    p2=-(4*P2*x)/(x^2+ro^2)^3;
+    p2=-(4*P3*x)/(x^2+ro^2)^3;
 
     res=w*(p1+p2);
 end
 function res=dv2(arg)
-    x=arg(1);ro=arg(2);w=arg(3);l=arg(4);h=arg(5);
-    
-    P1=R_1(w,ro,l);
-    P2=R_2_(w,ro,l,h);
+    x=arg(1);ro=arg(2);w=arg(3);P1=arg(6);P3=arg(8);
     
     tmp=(1/(ro^2*((x^2)/(ro^2)+1))+(ro^2-x^2)/(x^2+ro^2)^2);
-    p2=(P2/(2*ro^2))*tmp;
+    p2=(P3/(2*ro^2))*tmp;
     p3=P1/(ro^2*((x^2)/(ro^2)+1));
     
     res=w*(p2+p3);
@@ -271,15 +263,20 @@ end
 function res=R_1(w,ro,l)
     res=(w^2-l*ro^2)/(2*w^2);
 end
-function res=R_2(w,ro,l,h)
-%         eto L!
-    p=R_1(w,ro,l);
+function res=R_2(w,ro,h,p)
     P1=(h*ro^2)/(2*w^2);
     P2=p*(1-ro^2-p/2);
     P3=-(2*ro^2-1)/(4*w^2);
     res=P1+P2+P3;
 end
-function res=R_2_(w,ro,l,h)
-    res=R_2(w,ro,l,h)+R_1(w,ro,l)*ro^2;
+function res=R_2_(ro,p1,p2)
+    res=p2+p1*ro^2;
 end   
+function res=set_R(w,ro,l,h)
+    P1=R_1(w,ro,l);
+    P2=R_2(w,ro,h,P1);
+    P3=R_2_(ro,P1,P2);
+    
+    res=[P1 P2 P3];
+end
 %%                                             end of main functions                             
